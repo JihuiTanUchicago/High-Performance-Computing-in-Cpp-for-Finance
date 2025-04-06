658d4629c19abc45d47de272a8e06d60f1aa17bb
JihuiTanUchicago <tanjihui@uchicago.edu>
2025-04-06 16:58:42 -0700

    monte_carlo_pi_estimator implementation

---
+++ monte_carlo_pi_estimator/README.md
@@ -0,0 +1,24 @@
+### Compilation
+Compiled with: 
+```
+clang++ -std=c++17 -O2 -march=native monte_carlo_pi_estimator.cpp -o monte_carlo_pi_estimator_tr
+ial_size_<TRIAL_SIZE>
+```
+
+### Trial Size = 100 Output
+```
+./monte_carlo_pi_estimator_trial_size_100 
+Pi estimate: 3.28
+```
+
+### Trial Size = 1000 Output
+```
+./monte_carlo_pi_estimator_trial_size_1000
+Pi estimate: 3.108
+```
+
+### Trial Size = 10000 Output
+```
+./monte_carlo_pi_estimator_trial_size_10000
+Pi estimate: 3.138
+```
---
+++ monte_carlo_pi_estimator/monte_carlo_pi_estimator.cpp
@@ -0,0 +1,48 @@
+#include <chrono>
+#include <random>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+constexpr int TRIAL_COUNT = 10000;
+
+inline vector<double> generate_point(uniform_real_distribution<>& dis, mt19937& gen) {
+    double x = dis(gen);
+    double y = dis(gen);
+    return {x, y};
+}
+
+inline bool is_in_circle(vector<double> point) {
+    double distance_to_origin = sqrt(point[0] * point[0] + point[1] * point[1]);
+    if (distance_to_origin > 1.0) {
+        return false;
+    }
+    return true;
+}
+
+/*
+Calculating pi using monte carlo method.
+Assume circle has radius 1 within a square of side length 2.
+Assume circle origin is (0,0).
+*/
+int main(){
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_real_distribution<> dis(-1.0, 1.0);
+
+    int total_point_in_circle_count = 0;
+
+    for (int i = 0; i < TRIAL_COUNT; i++){
+        vector<double> point = generate_point(dis, gen);
+        if (is_in_circle(point)) {
+            total_point_in_circle_count++;
+        }
+    }
+
+    double pi_estimate = 4.0 * total_point_in_circle_count / TRIAL_COUNT;
+    cout << "Pi estimate: " << pi_estimate << endl;
+
+    return 0;
+}
